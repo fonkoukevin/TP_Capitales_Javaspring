ed8d2f3d22565de3532717a28066ee4b387245b7
fonkou <kevinfonkou09@gmail.com>
2024-05-29 15:52:54 +0200

    add player_scores

--- Desktop/countries/src/main/java/countries/countries/controller/CountryController.java
+++ Desktop/countries/src/main/java/countries/countries/controller/CountryController.java
@@ -1,11 +1,14 @@
 package countries.countries.controller;
 
 import countries.countries.model.Country;
+import countries.countries.model.Player;
+import countries.countries.model.Score;
 import countries.countries.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -16,26 +19,47 @@ public class CountryController {
     @Autowired
     private CountryService countryService;
 
-    @GetMapping("/start")
-    public Country getRandomCountry() {
+    @PostMapping("/start")
+    public Country getRandomCountry(@RequestBody Map<String, String> payload) {
+        String username = payload.get("username");
+        Optional<Player> playerOptional = countryService.getPlayerByUsername(username);
+        if (playerOptional.isEmpty()) {
+            countryService.createPlayer(username);
+        }
         return countryService.getRandomCountry();
     }
 
     @PostMapping("/{id}")
     public Map<String, String> guessCapital(@PathVariable Long id, @RequestBody Map<String, String> payload) {
         String guessedCapital = payload.get("capital");
+        String username = payload.get("username");
         Map<String, String> response = new HashMap<>();
         Optional<Country> countryOptional = countryService.getCountryById(id);
-        if (countryOptional.isPresent()) {
+        Optional<Player> playerOptional = countryService.getPlayerByUsername(username);
+        if (countryOptional.isPresent() && playerOptional.isPresent()) {
             Country country = countryOptional.get();
+            Player player = playerOptional.get();
             if (country.getCapital().equalsIgnoreCase(guessedCapital)) {
                 response.put("result", "Correct! You guessed the right capital.");
+                countryService.saveScore(player, true);
             } else {
                 response.put("result", "Wrong! The correct capital is " + country.getCapital());
+                countryService.saveScore(player, false);
             }
         } else {
-            response.put("result", "Country not found.");
+            response.put("result", "Country or player not found.");
         }
         return response;
+    }
+
+    @GetMapping("/score/{username}")
+    public List<Score> getPlayerScores(@PathVariable String username) {
+        Optional<Player> playerOptional = countryService.getPlayerByUsername(username);
+        if (playerOptional.isPresent()) {
+            Player player = playerOptional.get();
+            return countryService.getScoresByPlayerId(player.getId());
+        } else {
+            return null;
+        }
     }
 }
--- Desktop/countries/src/main/java/countries/countries/service/CountryService.java
+++ Desktop/countries/src/main/java/countries/countries/service/CountryService.java
@@ -1,19 +1,32 @@
 package countries.countries.service;
 
-
+import countries.countries.model.Player;
+import countries.countries.model.Score;
+import countries.countries.repository.PlayerRepository;
+import countries.countries.repository.ScoreRepository;
 import countries.countries.model.Country;
 import countries.countries.repository.CountryRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
 
     @Autowired
     private CountryRepository countryRepository;
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private ScoreRepository scoreRepository;
 
     private List<Country> remainingCountries;
 
@@ -43,5 +56,35 @@ public class CountryService {
         List<Country> countries = countryRepository.findAll();
         remainingCountries = new ArrayList<>(countries);
         Collections.shuffle(remainingCountries);
+    }
+
+    public Player createPlayer(String username) {
+        Player player = new Player();
+        player.setUsername(username);
+        return playerRepository.save(player);
+    }
+
+    public Optional<Player> getPlayerByUsername(String username) {
+        return playerRepository.findByUsername(username);
+    }
+
+    public Score saveScore(Player player, boolean correct) {
+        List<Score> scores = scoreRepository.findByPlayerId(player.getId());
+        Score score;
+        if (scores.isEmpty()) {
+            score = new Score();
+            score.setPlayer(player);
+        } else {
+            score = scores.get(0);
+        }
+        if (correct) {
+            score.setCorrectAnswers(score.getCorrectAnswers() + 1);
+            score.setScore(score.getScore() + 1);
+        }
+        return scoreRepository.save(score);
+    }
+
+    public List<Score> getScoresByPlayerId(Long playerId) {
+        return scoreRepository.findByPlayerId(playerId);
     }
 }
